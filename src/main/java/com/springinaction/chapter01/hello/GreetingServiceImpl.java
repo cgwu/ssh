2ccfb8bd7a18f913f4b490ffdaedf40030533171
cc <gg.wcg@qq.com>
2013-01-11 01:32:45 +0800

    a

--- src/main/java/com/springinaction/chapter01/hello/GreetingServiceImpl.java
+++ src/main/java/com/springinaction/chapter01/hello/GreetingServiceImpl.java
@@ -1,28 +1,39 @@
 package com.springinaction.chapter01.hello;
 
-public class GreetingServiceImpl implements GreetingService {
+import org.springframework.beans.factory.BeanNameAware;
 
-    private String greeting;
+public class GreetingServiceImpl implements GreetingService, BeanNameAware {
 
-    public GreetingServiceImpl() {
-    }
+	private String greeting;
 
-    public GreetingServiceImpl(String greeting) {
-	this.greeting = greeting;
-    }
+	private String beanName;
 
-    public String getGreeting() {
-	return greeting;
-    }
+	public String getBeanName() {
+		return beanName;
+	}
 
-    public void setGreeting(String greeting) {
-	this.greeting = greeting;
-    }
+	public GreetingServiceImpl() {
+	}
 
-    
-    public void sayGreeting() {
-	System.out.println(greeting);
-    }
+	public GreetingServiceImpl(String greeting) {
+		this.greeting = greeting;
+	}
 
-    
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public void setGreeting(String greeting) {
+		this.greeting = greeting;
+	}
+
+	public void sayGreeting() {
+		System.out.println("BeanName:"+this.beanName+","+greeting);
+	}
+
+	public void setBeanName(String paramString) {
+		System.out.println("Bean Name:" + paramString);
+		this.beanName = paramString;
+	}
+
 }
