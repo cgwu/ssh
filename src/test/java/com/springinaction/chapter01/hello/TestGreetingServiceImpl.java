a27b6bd9357055ea800959f673ba2c66db86ecee
cg <superwcg@gmail.com>
2013-01-10 11:25:16 +0800

    init project

---
+++ src/test/java/com/springinaction/chapter01/hello/TestGreetingServiceImpl.java
@@ -0,0 +1,24 @@
+package com.springinaction.chapter01.hello;
+
+import static org.junit.Assert.assertNotNull;
+
+import org.apache.log4j.Logger;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestGreetingServiceImpl {
+    static Logger log = Logger.getLogger(TestGreetingServiceImpl.class);
+    
+    @Test
+    public void testSayGreeting() {
+	log.info("testSayGreeting开始");
+	//ApplicationContext context = new ClassPathXmlApplicationContext("spring/hello.xml");
+	ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+	GreetingService service = context.getBean("greetingService", GreetingServiceImpl.class);
+	service.sayGreeting();
+	assertNotNull(service);
+	
+    }
+
+}
