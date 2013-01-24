dfc30b315d205da59fcb572ab6e890e8bdcc6a2a
cg <superwcg@gmail.com>
2013-01-24 13:23:08 +0800

    slf appender encoder

--- src/main/java/wu/cg/demo/slf/HelloWorld.java
+++ src/main/java/wu/cg/demo/slf/HelloWorld.java
@@ -8,12 +8,16 @@ import ch.qos.logback.core.util.StatusPrinter;
 
 public class HelloWorld {
     static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
-
+//    static final Logger logger = LoggerFactory.getLogger("ABC");
     public static void main(String[] args) {
 	logger.debug("Hello world.我的日志内容new");
-
+	doFoo();
 	// print internal state
-	LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
-	StatusPrinter.print(lc);
+//	LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
+//	StatusPrinter.print(lc);
+    }
+    
+    static void doFoo(){
+	logger.error("Foo发生");
     }
 }
