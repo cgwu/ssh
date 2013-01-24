3c2731bcb95df74f6d1caf098c4638000f005da9
cg <superwcg@gmail.com>
2013-01-24 14:58:49 +0800

    SLF4J 测试完毕. Console Appender, FileRollingAppender,SMTP Appender,ASYNC Appender

--- src/main/java/wu/cg/demo/slf/HelloWorld.java
+++ src/main/java/wu/cg/demo/slf/HelloWorld.java
@@ -3,21 +3,26 @@ package wu.cg.demo.slf;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.LoggerContext;
-import ch.qos.logback.core.util.StatusPrinter;
+//import ch.qos.logback.classic.LoggerContext;
+//import ch.qos.logback.core.util.StatusPrinter;
 
 public class HelloWorld {
-//    static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
-    static final Logger logger = LoggerFactory.getLogger("ABC");
+    static final Logger logger = LoggerFactory.getLogger(HelloWorld.class);
+    static final Logger logMail = LoggerFactory.getLogger("mail.logger");
     public static void main(String[] args) {
-	logger.error("Hello world.我的日志内容new");
+	for (int i = 0; i < 5; i++) {
+	    logMail.error("邮件日志测试,日志内容.{}+{}={}", i, i, i + i);
+	}
 	doFoo();
+	
+//	logger.info("done");
+	System.out.println(1+1);
 	// print internal state
 //	LoggerContext lc = (LoggerContext) LoggerFactory.getILoggerFactory();
 //	StatusPrinter.print(lc);
     }
     
     static void doFoo(){
-	logger.error("Foo发生");
+	logger.debug("Foo发生");
     }
 }
