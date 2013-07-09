1dac9599fedcd1eafdc8d6eeb50f29a9386bf9b0
cg <superwcg@gmail.com>
2013-07-09 18:31:00 +0800

    slf4j 2 log4j success

--- src/test/java/wu/cg/other/TestStringUtils.java
+++ src/test/java/wu/cg/other/TestStringUtils.java
@@ -10,26 +10,27 @@ import org.slf4j.LoggerFactory;
 public class TestStringUtils {
 
     static final Logger LOG = LoggerFactory.getLogger(TestStringUtils.class);
-    
+
     @Test
     public void test() {
-	String s=null;
-		
+	String s = null;
+
 	boolean bIsEmpty = StringUtils.isEmpty(s);
-	LOG.info("is empty? {}",bIsEmpty);
-	
+	LOG.info("is empty? {}", bIsEmpty);
+
 	s = "yes";
 	bIsEmpty = StringUtils.isEmpty(s);
-	LOG.info("is empty? {}",bIsEmpty);
-	
-	String sProcessed =  StringUtils.leftPad(s, 10,'0');
+	LOG.info("is empty? {}", bIsEmpty);
+
+	String sProcessed = StringUtils.leftPad(s, 10, '0');
 	System.out.println(sProcessed);
-	
-	String sRPad =  StringUtils.rightPad(s, 20,'#');
-	
-	String sPRadAbbrev = StringUtils.abbreviate(sRPad,10);
+
+	String sRPad = StringUtils.rightPad(s, 20, '#');
+
+	String sPRadAbbrev = StringUtils.abbreviate(sRPad, 10);
 	System.out.println(sPRadAbbrev);
-	
+
+	LOG.error("完成");
     }
 
 }
